3d55e684a649302b19796923670ea63ed0bc0ca3
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-17 17:24:08 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,11 +1,14 @@
-// supabase_client.js
-// Este arquivo será executado APÓS a biblioteca Supabase carregar
-if (typeof supabase === 'undefined') {
-    console.error("Supabase JS não carregada! Verifique a ordem dos scripts.");
-} else {
+// Espera a janela carregar completamente, garantindo que supabase esteja definido
+window.addEventListener('load', () => {
+    if (typeof supabase === 'undefined') {
+        console.error("Supabase JS não carregada! Verifique a ordem dos scripts.");
+        return;
+    }
+
     const supabaseUrl = 'https://vhwhjnghtmlrfieiwssi.supabase.co';
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU';
 
-    // Cria o cliente global
+    // Cliente global
     window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
-}
+    console.log("SupabaseClient carregado com sucesso!");
+});
