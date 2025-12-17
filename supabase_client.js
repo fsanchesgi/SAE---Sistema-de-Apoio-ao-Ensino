3e7637fbf3b88ea0d8198299bab1a7d0fadd23b1
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-17 17:18:29 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,10 +1,12 @@
 // supabase_client.js
-// Atenção: deve ser incluído APÓS a biblioteca Supabase
-var supabaseUrl = 'https://vhwhjnghtmlrfieiwssi.supabase.co';
-var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU';
+document.addEventListener("DOMContentLoaded", function() {
+    if (typeof supabase === 'undefined') {
+        console.error("Supabase JS não carregada! Verifique a ordem dos scripts.");
+        return;
+    }
 
-if (typeof supabase !== 'undefined') {
-    var supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
-} else {
-    console.error("Supabase JS não carregada! Verifique a ordem dos scripts.");
-}
+    var supabaseUrl = 'https://vhwhjnghtmlrfieiwssi.supabase.co';
+    var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU';
+
+    window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+});
