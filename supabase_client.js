09f5c3bc9854f9c5d6497f00f96d9627a1f3d607
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-18 22:21:07 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,4 +1,7 @@
-// Inicialização do Supabase
+// supabase_client.js
+// Inicialização do Supabase v2
 const supabaseUrl = "https://vhwhjnghtmlrfieiwssi.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU"; // chave anon pública
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub3QiLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU";
+
+// Cria cliente Supabase
+const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
