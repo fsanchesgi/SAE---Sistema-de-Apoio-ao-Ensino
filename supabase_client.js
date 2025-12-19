da29d02458fb4e2a4622eaaa659f57311898024a
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-19 19:14:22 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,4 +1,7 @@
+// Cliente Supabase usando CDN (SEM import, SEM module)
 
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js"></script>
-<script src="supabase_client.js"></script>
-<script src="login.js"></script>
+const supabaseUrl = "https://vhwhjnghtmlrfieiwssi.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU";
+
+// A CDN cria o objeto global "Supabase"
+const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
