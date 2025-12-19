b778e0e6117fd228a6925e46cc661beef4ab317e
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-19 19:17:58 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -4,4 +4,4 @@ const supabaseUrl = "https://vhwhjnghtmlrfieiwssi.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU";
 
 // A CDN cria o objeto global "Supabase"
-const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabaseJs.createClient(supabaseUrl, supabaseKey);
