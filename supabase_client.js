7ecbc999c418e3ba6b2c3c2f6d0d532e0263ccda
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-18 22:32:56 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,4 +1,3 @@
-<!-- NO HTML, antes do login.js -->
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js"></script>
 <script src="supabase_client.js"></script>
 <script src="login.js"></script>
