b9baee42068ca0839c7db704cff99b57ab645ca6
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-18 22:24:27 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -3,5 +3,5 @@
 const supabaseUrl = "https://vhwhjnghtmlrfieiwssi.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub3QiLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU";
 
-// Cria cliente Supabase
-const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
+// CORRETO: usar 'supabaseJs.createClient'
+const supabase = supabaseJs.createClient(supabaseUrl, supabaseKey);
