79dab3a7c030c9e79377788683c9a19c36575c20
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-18 22:29:27 -0300

    Update supabase_client.js

--- supabase_client.js
+++ supabase_client.js
@@ -1,9 +1,4 @@
-// supabase_client.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-// URL e chave pública do seu projeto Supabase
-const supabaseUrl = "https://vhwhjnghtmlrfieiwssi.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2hqbmdodG1scmZpZWl3c3NpIiwicm9sZSI6ImFub3QiLCJpYXQiOjE3NjU5OTM4OTUsImV4cCI6MjA4MTU2OTg5NX0.XV6vxBeRtIcDNEGsJuDU_wPcnm2qmK8ZoHEuHQR2TMU";
-
-// Cria o cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseKey);
+<!-- NO HTML, antes do login.js -->
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js"></script>
+<script src="supabase_client.js"></script>
+<script src="login.js"></script>
